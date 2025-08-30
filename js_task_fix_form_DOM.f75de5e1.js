68036a29105140b900a357795635f1242974c6e2
Vasyliev Vadym <protocol222333@gmail.com>
2025-08-30 15:25:10 +0300

    make build

---
+++ js_task_fix_form_DOM.f75de5e1.js
@@ -0,0 +1,19 @@
+'use strict';
+const inputs = [
+    ...document.querySelectorAll('form input')
+];
+inputs.forEach((input, index)=>{
+    const label = document.createElement('label');
+    label.classList.add('field-label');
+    if (!input.id) input.id = `${input.name || 'field'}-${index}`;
+    label.htmlFor = input.id;
+    label.textContent = capitalize(input.name);
+    input.placeholder = capitalize(input.name);
+    input.parentElement.insertBefore(label, input);
+});
+function capitalize(text) {
+    const capitalized = text[0].toUpperCase() + text.slice(1);
+    return capitalized;
+}
+
+//# sourceMappingURL=js_task_fix_form_DOM.f75de5e1.js.map
